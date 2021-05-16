99a4167b7518aa604ccae65a239571f3ec2f73c9
mayassililo <83997660+mayassililo@users.noreply.github.com>
2021-05-16 22:01:39 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,33 @@
+/*let myLogo = document.getElementById('myLogo');
+Document.getElementById(myButton).onclick=function(){
+    myLogo.classList.toggle("fade-in-image");
+}*/
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 100);
+}
+function kern(element){
+    var op=0.1;
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+            //element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 50);
+}
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
