7863fe5626b11953db64388112db3dd8479638f7
mayassililo <83997660+mayassililo@users.noreply.github.com>
2021-05-20 14:47:53 +0100

    Add files via upload
    aucun changement important mais bcp de recherches, les codes trouvé ont des problème à s'implémenter et fonctionner
    https://stackoverflow.com/questions/5670879/css-html-create-a-glowing-border-around-an-input-field
    https://stackoverflow.com/questions/31466146/div-animation-with-javascript-after-clicking-on-a-link-how
    https://stackoverflow.com/questions/50682483/smooth-scroll-to-specific-div-on-click-in-css

--- home.js
+++ home.js
@@ -1,35 +1,33 @@
+/*let myLogo = document.getElementById('myLogo');
+Document.getElementById(myButton).onclick=function(){
+    myLogo.classList.toggle("fade-in-image");
+}*/
 
-//fonction permettant de fade un element
 function fade(element) {
-  var op = 1; // initial opacity
-  var timer = setInterval(function () {
-    if (op <= 0.1) {
-      clearInterval(timer);
-      element.style.display = "none";
-    }
-    element.style.opacity = op;
-    element.style.filter = "alpha(opacity=" + op * 100 + ")";
-    op -= op * 0.1;
-  }, 100);
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 100);
 }
-
-
-//fonction permettant de unfade un element 
-function kern(element) {
-  var op = 0.1;
-  var timer = setInterval(function () {
-    if (op >= 1) {
-      clearInterval(timer);
-      //element.style.display = 'none';
-    }
-    element.style.opacity = op;
-    element.style.filter = "alpha(opacity=" + op * 100 + ")";
-    op += op * 0.1;
-  }, 50);
+function kern(element){
+    var op=0.1;
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+            //element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 50);
 }
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
 
-
-//fonction permettant de n3ess pendant wa7d delta T
-function sleep(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
